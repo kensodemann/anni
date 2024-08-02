0759ae50ab4b559c8a259cf8120a150f0445fbf4
Ken Sodemann <ken@ionic.io>
2024-08-02 14:04:36 -0500

    replicate the issue

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  segmentList = ["See what's new", 'Member Benefits', 'Discounts', 'Travel', 'Via'];
+  selectedSegment = this.segmentList[0];
 
   constructor() {}
-
 }
